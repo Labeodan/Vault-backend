2486983dbd127182559da6d77b34e125b6dd8f81
jsillero-ga <jsillerosalado@gmail.com>
2024-11-01 12:05:25 +0100

    Deployment changes

---
+++ netlify/functions/api.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const MongoStore = require("connect-mongo");
+const methodOverride = require('method-override')
+const path = require('path');
+const authRouter = require("./controllers/auth")
+const cors = require('cors')
+const serverless = require('serverless-http')
+
+
+const categoriesRouter = require("../../controllers/categories")
+const transactionRouter = require("../../controllers/transactions")
+const budgetRouter = require("../../controllers/budget")
+
+require('dotenv/config')
+
+// ! -- Variables
+const app = express()
+const port = 3000
+
+
+// ! -- Middleware
+app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Index view.")
+});
+
+
+//Controller
+app.use("/auth", authRouter)
+app.use("/category", categoriesRouter)
+app.use("/transactions", transactionRouter)
+app.use("/budget", budgetRouter)
+
+// Start the Express server
+const startServers = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Database connection established");
+
+         
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+startServers();
+
+module.exports.handler = serverless(app)
